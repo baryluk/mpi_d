119cd508cb8b8dcad6ef79803cfd49123104713d
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-09-01 16:59:16 +0200

    Add simple example, so we can test compiling. Nothing there yet.

---
+++ example1.d
@@ -0,0 +1,21 @@
+module example1;
+
+import mpi;
+
+/** Compile using
+ *
+ *   dmd2 example1.d mpi.d -L-lmpi
+ *
+ * Other compilers, dmd1, gdc and ldc all should work.
+ *
+ * You may need to set linker search paths, and LD_LIBRARY_PATH for runtime loading of proper libraries.
+ *
+ *  To see what library will be used exactly use
+ *
+ *   ldd example1
+ *
+ *
+ */
+
+void main() {
+}
